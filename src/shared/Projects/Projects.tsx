670fff3f9aee180f2c3b2104e1d97da1c4957041
deji <dejiwilliams9@gmail.com>
2025-02-21 17:05:37 +0100

    new commit.

--- src/shared/Projects/Projects.tsx
+++ src/shared/Projects/Projects.tsx
@@ -35,7 +35,7 @@ const Projects = ():React.ReactElement=>{
       accessVariant:isTab?'hover':'static',
       code_url:'#',
       website_url:'https://intellisight-two.vercel.app/',
-      stacks:['REACT JS','Context Api', 'TailwindCss'],
+      stacks:['Next JS','Context Api', 'TailwindCss'],
       projectName:'CoinTable with darkmode.',
       projectImage: table
     },    
